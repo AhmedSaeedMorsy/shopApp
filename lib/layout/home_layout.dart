f17bc10c15b31484e11646a6a039d205c3fa6829
Ahmed Saeed Morsy <ahmedsaidmorsy74@gmail.com>
2022-04-18 02:02:21 +0200

    new bottom nav bar

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -2,59 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopapp/modules/search/search.dart';
 import 'package:shopapp/shared/componant/componant.dart';
+import 'package:shopapp/shared/constant.dart';
 import 'package:shopapp/shared/cubit/app_cubit/shop_cubit/cubit.dart';
 import 'package:shopapp/shared/cubit/app_cubit/shop_cubit/satates.dart';
-
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class HomeLayOut extends StatelessWidget {
   const HomeLayOut({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<ShopCubit,ShopStates>(listener: (context, state) {},builder: (context, state){
+    return BlocConsumer<ShopCubit, ShopStates>(
+      listener: (context, state) {},
+      builder: (context, state) {
         var cubit = ShopCubit.get(context);
-      
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text("Cesta"),
-          actions: [
-            IconButton(
-              onPressed: () {
-                navigatTo(context, Search());
-              },
-              icon: const Icon(Icons.search_outlined),
-            ),
-          ],
-        ),
-        body: cubit.bottomnavItem[cubit.currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Cesta"),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  navigatTo(context, Search());
+                },
+                icon: const Icon(Icons.search_outlined),
+              ),
+            ],
+          ),
+          body: cubit.bottomnavItem[cubit.currentIndex],
+          bottomNavigationBar: CurvedNavigationBar(
+            color: mainColor,
+            backgroundColor: Colors.white12,
+            items: const [
+              Icon(Icons.home_outlined, size: 30,color: Colors.white,),
+              Icon(
                 Icons.apps_outlined,
+                color: Colors.white,
+                size: 30,
               ),
-              label: "categories",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
+              Icon(
                 Icons.favorite_outline,
+                color: Colors.white,
+                size: 30,
               ),
-              label: "favrites",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
+              Icon(
                 Icons.settings,
+                color: Colors.white,
+                size: 30,
               ),
-              label: "settings",
-            ),
-        ],
-        currentIndex: cubit.currentIndex,
-        onTap: (index) =>  cubit.changeIndex(index),
-      ),
+            ],
+            onTap: (index) => cubit.changeIndex(index),
+          ),
+        );
+      },
     );
-    },);
   }
 }
